91a42319002ace858947441136722d494b818c91
M-Shahin <shahin.sarker004@gmail.com>
2020-02-27 13:27:20 +1100

    FixedMoviesSaveIssue

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -47,5 +47,25 @@ namespace Vidly.Controllers
 
             return View("MovieForm",viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.MovieTypeId = movie.MovieTypeId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.MovieType = movie.MovieType;                
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
